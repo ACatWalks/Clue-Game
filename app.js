9e8a534ff701ab7b9f31ff9161c21677536f8595
Amanda Woods <Amanda@amandas-mbp.attlocal.net>
2022-02-09 19:56:31 -0800

    First commit, basic HTML set up and psuedocode in js file

---
+++ app.js
@@ -0,0 +1,33 @@
+//Create card deck using 4 arrays: Suspects, Weapons, Rooms, and Cards Available
+
+//Take one card from each deck and place it in a secret envelope
+
+//Create user object with cards, distance traveled, and room
+
+//Deal cards to user
+
+//Automatically check off dealt cards on notebook
+
+//Deal cards to computer
+
+//Create function to simulate dice rolling
+
+//Come up with distances between rooms
+
+//Add event listener to "Move To..." button to allow user to move
+
+//Simulate computer moving
+
+//Keep track of whose turn it is
+
+//Add event listener to "Secret Passageway" button
+
+//Add event listener to "Make a Suggestion" button
+
+//Select and show user cards to refute his/her suggestion if possible
+
+//Simulate computer crossing off cards on its notebook (not visible to user)
+
+//Add event listener to "Make an Accusation" button
+
+//Simulate computer making accusation
